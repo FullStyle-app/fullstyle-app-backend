ae09bf8c1f66ab99cbb8f1cf6fadf7c5a7f82e2a
Pauline Cvt <paulinecvt2021@gmail.com>
2024-03-05 18:09:39 +0100

    installed packages + npm install on local machine for Pauline + took notes of all the dependencies needed + env variable

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const app = require("./app");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const express = require("express");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5005
 const PORT = process.env.PORT || 5005;
@@ -6,3 +12,10 @@ const PORT = process.env.PORT || 5005;
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
 });
+
+// to import : 
+// npm install mongoose P
+// npm install express P
+// npm install cors P
+// npm install bcrypt P
+// npm install jsonwebtoken P
